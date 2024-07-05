c03b823138078ab92548175c62f7c9d67eaf84af
TheVasuA <thevasu.a@gmail.com>
2024-07-05 22:36:59 +0530

    null value removed from array

--- src/ThingsData.jsx
+++ src/ThingsData.jsx
@@ -3,28 +3,38 @@ import axios from 'axios';
 
 const ThingsData = () => {
     const [thingsdata, setThingsdata] = useState([]);
+    const [filterData, setFilteredData] = useState([]);
     const url2='https://api.thingspeak.com/channels/2589884/feeds.json'
+
+  
     const handlegetClick = async () => {
-        try {
-          
-          const response = await axios.get(url2);
-          setThingsdata(response.data.feeds);
-        } catch (error) {
-          console.error('Error sending data:', error);
-        }
-      };
-      useEffect(()=>{
-        const intervalId = setInterval(() => {
-            handlegetClick()
-          }, 1000) 
-          return () => clearInterval(intervalId)
-        
-      },[])
+    await axios.get(url2)
+    .then(response => {
+        const responseData = response.data.feeds;
+        const nonNullObjects = responseData.filter(item => 
+          Object.values(item).every(value => value !== null)
+        );
+        setThingsdata(responseData);
+        setFilteredData(nonNullObjects);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    };
+
+    useEffect(()=>{
+      const intervalId = setInterval(() => {
+          handlegetClick()
+        }, 1000) 
+        return () => clearInterval(intervalId)
+      
+    },[])
+
   return (
-    <div>      {thingsdata && (
+    <div>      {filterData && (
         <div className=" p-2 bg-yellow-100 text-yellow-300 rounded-md">
     <table >
-          {thingsdata.slice(-1).reverse().map((data)=>(
+          {filterData.slice(-1).reverse().map((data)=>(
           <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 flex flex-col"  key={data.entry_id}>
                  
             <td className="px-6 py-4">{data.field1}</td>
